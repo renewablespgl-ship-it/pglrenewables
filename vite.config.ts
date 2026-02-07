ecd5f3d931f5be0e2a3756074d03203a8603c60f
renewablespgl-ship-it <renewablespgl@gmail.com>
2026-02-07 12:09:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  // ✅ REQUIRED for GitHub Pages
   base: "/pglrenewables/",
 
   server: {
